f477b1c6e6b019738d2d74d8de9a1639fd5e4147
OLU-TITUS <bamidt1@gmail.com>
2022-12-14 12:30:41 -0800

    Absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - Check absolute
+ *  @b: an integer
+ *  Description: This script return absolute value of a number
+ *  Return: Absolute value of b
+ */
+
+int _abs(int b)
+{
+	if (b >= 0)
+		return (b);
+	else
+		return (b * -1);
+}
