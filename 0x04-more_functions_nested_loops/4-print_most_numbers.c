96ed651c7b01459828ea830c50790981a572874f
OLU-TITUS <bamidt1@gmail.com>
2022-12-16 13:41:44 -0800

    Print numbers with some exceptions

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,9 +1,7 @@
 #include "main.h"
 
 /**
- * print_most_numbers - Print numbers from (0-9)
- *
- *
+ * print_most_numbers - Print numbers from 0-9 except for 2 and 4.
  */
 
 void print_most_numbers(void)
@@ -14,7 +12,7 @@ void print_most_numbers(void)
 	{
 		if (n != 2 && n != 4)
 		{
-			_putchar((n % 10) + '10');
+			_putchar((n % 10) + '0');
 		}
 	}
 	_putchar('\n');
