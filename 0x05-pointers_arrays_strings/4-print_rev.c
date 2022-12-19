5cf7439744be91d85339ac65b809b407ca9e6760
OLU-TITUS <bamidt1@gmail.com>
2022-12-19 13:24:33 -0800

    A reverse string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_rev - A function that print string in reverse
+ * @s: An input integer
+ * Return: Nothing
+ */
+
+void print_rev(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+		while (len)
+			_putchar(s[--len]);
+
+		_putchar('\n');
+}
