a84b038c01d85ebd7819544a2efefaa7f7d03a1f
OLU-TITUS <bamidt1@gmail.com>
2022-12-19 16:33:31 -0800

    Half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,19 +7,19 @@
 
 void puts_half(char *str)
 {
-	int len = 0;
+	int len = 0, i, n;
 
-	while (*str != '\0')
-	{
+	while (str[len] != '\0')
+
 		len++;
-		str++;
-	}
 
-	str -= (len / 2);
-	while (*str != '\0')
-	{
-		_putchar(*str);
-		str++;
-	}
+	if (len % 2 == 0)
+		n = len / 2;
+
+	else
+		n = (len + 1) / 2;
+	for (i = n; i < len; i++)
+		_putchar(str[i]);
+
 	_putchar('\n');
 }
