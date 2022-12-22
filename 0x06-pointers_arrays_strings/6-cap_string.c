094667cc733f140625505f3b426022569653de98
OLU-TITUS <bamidt1@gmail.com>
2022-12-22 12:21:08 -0800

    Capitalize strings

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,4 +1,4 @@
-int check_seperators(char c);
+#include "main.h"
 
 /**
  * cap_string - Function capitalize all words of a string
@@ -8,36 +8,26 @@ int check_seperators(char c);
 
 char *cap_string(char *s)
 {
-	int i = 0;
+	int index = 0;
 
-	while (s[i])
+	while (s[++index])
 	{
-		if (i == 0 && (s[i] >= 'a' && s[i] <= 'z'))
-			s[i] -= 32;
-
-		if (check_seperators(s[i]) && (s[i + 1] >= 'a' && s[i + 1] <= 'z'))
-			s[i + 1] -= 32;
-		i++;
+		while (!(s[index] >= 'a' && s[index] <= 'z'))
+			index++;
+		if (s[index - 1] == ' ' ||
+				s[index - 1] == '\t' ||
+				s[index - 1] == '\n' ||
+				s[index - 1] == ',' ||
+				s[index - 1] == ';' ||
+				s[index - 1] == '.' ||
+				s[index - 1] == '!' ||
+				s[index - 1] == '?' ||
+				s[index - 1] == '"' ||
+				s[index - 1] == '(' ||
+				s[index - 1] == ')' ||
+				s[index - 1] == '{' ||
+				s[index - 1] == '}')
+			s[index] -= 32;
 	}
 	return (s);
-}
-
-/**
- * check_seperators - Seperators of words; space, tabulation, new line,
- * ,, ;, ., ?, ", (, ), {, and }
- * @c: an input character
- * Return: 1 if seperator, 0 otherwise
- */
-
-int check_seperators(char c)
-{
-	int i = 0;
-	char seperators[13] = { ' ', '\t', '\n', ',', ';', '.', '!', '?',
-		'"', '(', ')', '{', '}' };
-	for (; i < 13; i++)
-	{
-		if (c == seperator[i])
-			return (1);
-	}
-	return (0);
 }
